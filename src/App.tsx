03aeae7f34072d4fb6aeaab54c98ec0ed2fa7aa9
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-05-04 16:14:56 -0400

    ADDED useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,24 @@ import { Form } from './components/Form'
 import './App.css';
 
 function App() {
+
+const [serverData, setServerData] = React.useState([])
+
+React.useEffect(() => {
+  fetch('http://44.212.63.82:4000')
+  // fetch('http://localhost:4000')
+    .then(res => res.json())
+    .then(data => setServerData(data))
+    .catch(err => console.error(err))
+}, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <Hello firstName= 'Véronie' lastName='Cazeau' />
         <Form /> 
+        {/* @ts-ignore */}
+        {serverData && serverData.map(eachDog => <p>{eachDog.name} - {eachDog.age}</p>)}
       </header>
     </div>
   );
